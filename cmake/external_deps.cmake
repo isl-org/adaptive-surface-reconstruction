8be6dcb5b3dc0107505e5c68ea2738a1fb2ea003
Benjamin Ummenhofer <benjaminum@gmail.com>
2021-10-15 19:47:44 +0200

    make patchelf a required program

--- cmake/external_deps.cmake
+++ cmake/external_deps.cmake
@@ -6,18 +6,20 @@ find_package( Python COMPONENTS Interpreter )
 # show downloads during configure step
 set( FETCHCONTENT_QUIET OFF CACHE BOOL "" FORCE )
 
-find_program( patchelf_binary patchelf )
+find_program( patchelf_binary patchelf REQUIRED )
 
-# download pybind11
-FetchContent_Declare(
-        pybind11
-        URL "https://github.com/pybind/pybind11/archive/v2.3.0.tar.gz"
-)
-FetchContent_GetProperties(pybind11)
-if(NOT pybind11_POPULATED)
-  message( STATUS "Populating pybind11" )
-  FetchContent_Populate(pybind11)
-  add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BINARY_DIR})
+if( BUILD_PYTHON_MODULE )
+        # download pybind11
+        FetchContent_Declare(
+                pybind11
+                URL "https://github.com/pybind/pybind11/archive/v2.3.0.tar.gz"
+        )
+        FetchContent_GetProperties(pybind11)
+        if(NOT pybind11_POPULATED)
+        message( STATUS "Populating pybind11" )
+        FetchContent_Populate(pybind11)
+        add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BINARY_DIR})
+        endif()
 endif()
 
 
